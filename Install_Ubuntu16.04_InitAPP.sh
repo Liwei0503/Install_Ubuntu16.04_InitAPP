64f4f39ad7755103019ea5fdfa22082d80d0df6d
Liwei0503 <liwei0503@gmail.com>
2018-12-24 16:13:38 +0800

    Update Install_Ubuntu16.04_InitAPP.sh

--- Install_Ubuntu16.04_InitAPP.sh
+++ Install_Ubuntu16.04_InitAPP.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y nano vim git cmake tree chromium-browser
-sudo apt-get install -y git make gcc ant openjdk-8-jdk unzip golang-go
-
+sudo apt-get install -y git make gcc ant openjdk-8-jdk unzip golang-go unzip
+sudo pip install python-can
+sudo apt-get -y install python-pip
 
