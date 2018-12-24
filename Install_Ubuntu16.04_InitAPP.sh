95bf32b1fe7164991d53d4f5bcaa64dd109da851
Liwei0503 <liwei0503@gmail.com>
2018-12-24 16:14:36 +0800

    Update Install_Ubuntu16.04_InitAPP.sh

--- Install_Ubuntu16.04_InitAPP.sh
+++ Install_Ubuntu16.04_InitAPP.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y nano vim git cmake tree chromium-browser
 sudo apt-get install -y git make gcc ant openjdk-8-jdk unzip golang-go unzip
 sudo pip install python-can
 sudo apt-get -y install python-pip
-
+sudo apt-get -y install can-utils
