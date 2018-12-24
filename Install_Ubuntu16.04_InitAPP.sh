a3cb4f397dba15da5d8a95d7fa2ccd32e151a629
Liwei0503 <liwei0503@gmail.com>
2018-12-24 16:38:14 +0800

    Update Install_Ubuntu16.04_InitAPP.sh

--- Install_Ubuntu16.04_InitAPP.sh
+++ Install_Ubuntu16.04_InitAPP.sh
@@ -5,3 +5,4 @@ sudo apt-get install -y git make gcc ant openjdk-8-jdk unzip golang-go unzip
 sudo pip install python-can
 sudo apt-get -y install python-pip
 sudo apt-get -y install can-utils
+bash <(curl -s https://thonny.org/installer-for-linux)
