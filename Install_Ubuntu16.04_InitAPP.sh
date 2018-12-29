1f1e44d1d2cf32e738e29b359535d090f0071ab2
Liwei0503 <liwei0503@gmail.com>
2018-12-29 11:33:56 +0800

    Update Install_Ubuntu16.04_InitAPP.sh

--- Install_Ubuntu16.04_InitAPP.sh
+++ Install_Ubuntu16.04_InitAPP.sh
@@ -1,8 +1,9 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y nano vim git cmake tree chromium-browser curl
+sudo apt-get install -y nano vim git cmake tree chromium-browser curl wget
 sudo apt-get install -y git make gcc ant openjdk-8-jdk unzip golang-go unzip gddrescue xz-utils gnome-disk-utility
 sudo pip install python-can
 sudo apt-get -y install python-pip
 sudo apt-get -y install can-utils
+sudo wget https://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
 bash <(curl -s https://thonny.org/installer-for-linux)
